6ef4568a8477fa8df3ae2f3d6fe7652abf37c97b
Aaron Sadler <aaron@umbhost.net>
2025-10-31 12:38:22 +0000

    Updated to Umbraco 17-RC1

--- src/HCS.Meta.Robots/RoutingHelper.cs
+++ src/HCS.Meta.Robots/RoutingHelper.cs
@@ -6,6 +6,9 @@ using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Extensions;
 using System.Diagnostics.CodeAnalysis;
+#if NET10_0
+using Umbraco.Cms.Core.Services.Navigation;
+#endif
 
 namespace HCS.Meta.Robots;
 
@@ -16,16 +19,37 @@ public static class RoutingHelper
         var umbracoContext = GetUmbracoContext(context);
         var domain = DomainUtilities.SelectDomain(umbracoContext?.Domains?.GetAll(false), umbracoContext.CleanedUmbracoUrl);
 
+#if NET10_0
+        var documentNavigationQueryService = GetIDocumentNavigationQueryService(context);
+        if (domain == null)
+            return DefaultToFirstRoot(documentNavigationQueryService, umbracoContext);
+
+        var content = umbracoContext.Content?.GetById(domain.ContentId);
+        return content ?? DefaultToFirstRoot(documentNavigationQueryService, umbracoContext);
+#else
         if (domain == null)
             return DefaultToFirstRoot(umbracoContext);
 
         var content = umbracoContext.Content?.GetById(domain.ContentId);
         return content ?? DefaultToFirstRoot(umbracoContext);
+#endif
     }
 
+#if NET10_0
+    private static IPublishedContent DefaultToFirstRoot(IDocumentNavigationQueryService documentNavigationQueryService, IUmbracoContext umbracoContext)
+    {
+        var hasRootKeys = documentNavigationQueryService.TryGetRootKeys(out var rootKeys);
+        if (hasRootKeys)
+        {
+            return umbracoContext.Content.GetById(rootKeys.FirstOrDefault()) ?? new DummyIPublishedContent();
+        }
+
+        return new DummyIPublishedContent();
+    }
+#else
     private static IPublishedContent DefaultToFirstRoot(IUmbracoContext umbracoContext)
         => umbracoContext.Content?.GetAtRoot().FirstOrDefault() ?? new DummyIPublishedContent();
-
+#endif
 
     [return: NotNull]
     private static IUmbracoContext GetUmbracoContext(ActionContext actionContext)
@@ -34,4 +58,14 @@ public static class RoutingHelper
 
         return umbracoContext ?? throw new InvalidOperationException("Umbraco Context is null");
     }
+
+#if NET10_0
+    [return: NotNull]
+    private static IDocumentNavigationQueryService GetIDocumentNavigationQueryService(ActionContext actionContext)
+    {
+        var documentNavigationQueryService = actionContext.HttpContext.RequestServices.GetService<IDocumentNavigationQueryService>();
+
+        return documentNavigationQueryService ?? throw new InvalidOperationException("IDocumentNavigationQueryService is null");
+    }
+#endif
 }
